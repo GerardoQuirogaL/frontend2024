113aacfe155f0563e96e462645ee206c7c2fc02d
Gerardo Luis Quiroga León <gerardoquiroga718@gmail.com>
2024-04-15 11:55:48 -0600

    Creacion de un nuevo proyecto cosas wue hacer TO DO LIST

---
+++ todolist/src/App.jsx
@@ -0,0 +1,46 @@
+
+function App() {
+
+  return (
+    <div class="container">
+      <h1>To Do List</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-6">
+          <h4>Tasks</h4>
+        </div>
+
+        <div className="col-2">
+          <h4>Limit</h4>
+        </div>
+
+        <div className="col">
+          <h4>Actions</h4>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          Estudiar
+        </div>
+
+        <div className="col-2">
+          13:00
+        </div>
+
+        <div className="col">
+          <button className="btn btn-sm btn-primary">
+          Edit
+          </button>
+          <button className="btn btn-sm btn-danger">
+            Delete
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
